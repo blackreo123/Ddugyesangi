42eb36499db68df03d9fd3708020e293c763cc85
JIHA YOON <jihayoon@JIHAui-MacBookPro.local>
2025-08-17 18:49:24 +0900

    광고 디버그랑 릴리즈 나누기

--- Ddugyesangi/Views/BannerAdView.swift
+++ Ddugyesangi/Views/BannerAdView.swift
@@ -4,11 +4,13 @@ import GoogleMobileAds
 struct BannerAdView: UIViewRepresentable {
     func makeUIView(context: Context) -> GADBannerView {
         let bannerView = GADBannerView(adSize: GADAdSizeBanner)
+#if DEBUG
         // 테스트 광고 ID
-//        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+#elseif release
         // 본방
         bannerView.adUnitID = "ca-app-pub-7521928283190614/6447748065"
-//        bannerView.rootViewController = UIApplication.shared.windows.first?.rootViewController
+#endif
         
         if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
            let rootViewController = windowScene.windows.first?.rootViewController {
--- Ddugyesangi/Views/Part/ProgressBarView.swift
+++ Ddugyesangi/Views/Part/ProgressBarView.swift
@@ -1,5 +1,5 @@
 //
-//  CircularProgressView.swift
+//  ProgressBarView.swift
 //  Ddugyesangi
 //
 //  Created by JIHA YOON on 2025/08/16.
