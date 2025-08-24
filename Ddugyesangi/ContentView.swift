f82bdcc99d5f0ab868cc6da7cb4d3ddb1a1efd49
JIHA YOON <jihayoon@JIHAui-MacBookPro.local>
2025-08-24 09:08:35 +0900

    아이패드 지원

--- Ddugyesangi/ContentView.swift
+++ Ddugyesangi/ContentView.swift
@@ -2,8 +2,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        // TODO: navigationViewStyle이 사라질 예정이므로 다른 표현 방법을 찾기
         NavigationView {
             ProjectListView()
         }
+        .navigationViewStyle(.stack)
     }
 }
